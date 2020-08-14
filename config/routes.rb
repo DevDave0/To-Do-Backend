557f85b6326b8feef79ce513014524e4ccedc776
devdave0 <dbchung24@gmail.com>
2020-08-14 14:37:26 -0400

    added actions to user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :tasklists
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'auth#create'
+
 end
