97b36ae025e45adcda3db4ce0083df09dbe37ca2
devdave0 <dbchung24@gmail.com>
2020-08-17 16:57:11 -0400

    small changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
 
+    skip_before_action :logged_in?, only: [:create, :index]
+
     def index 
         @tasks = Task.all 
         render json: @tasks
