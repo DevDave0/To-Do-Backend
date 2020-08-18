33e63e9c83772177934ac450207f94afa86c87c7
devdave0 <dbchung24@gmail.com>
2020-08-18 16:23:55 -0400

    added delete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
-    skip_before_action :logged_in?, only: [:create, :index]
+    skip_before_action :logged_in?, only: [:create, :index, :destroy]
 
     def index 
         @tasks = Task.all 
@@ -12,6 +12,12 @@ class TasksController < ApplicationController
         @task = Task.create(strong_params)
         render json: @task, status: :created
     end
+
+    def destroy 
+        @task = Task.find(params[:id])
+        @task.destroy 
+        render json: @task
+    end 
 
 
     private
